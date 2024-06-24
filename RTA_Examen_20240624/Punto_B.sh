1b3bb568e00932860071f9b18de50499d014c9ae
nahuelglalin <nahuelglalin@gmail.com>
2024-06-24 20:29:01 -0300

    fix

---
+++ RTA_Examen_20240624/Punto_B.sh
@@ -0,0 +1,9 @@
+ 1095  sudo touch /usr/local/bin/LalinAltaUser-Groups.sh
+ 1096  cat /usr/local/bin/LalinAltaUser-Groups.sh
+ 1097  clear
+ 1098  cat /usr/local/bin/LalinAltaUser-Groups.sh
+ 1099  bash /usr/local/bin/LalinAltaUser-Groups.sh nano UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt 
+ 1100  bash Scripts/obtener_usuarios_y_grupos.sh 
+ 1101  tree /work/
+ 1102  sudo tree /work/
+ 1103  echo "---- Fin Punto B ----"
